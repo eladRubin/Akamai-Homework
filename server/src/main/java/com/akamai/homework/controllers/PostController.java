9ec7d9fd0fa4b1c9165621d63ad5a1d60ca4e9bd
eladrubin <eladrubin1@gmail.com>
2024-05-29 16:33:15 +0300

    1. added paging and sorting mechanism 2. replace all returned Strings in controllers to ResponseEntity

--- server/src/main/java/com/akamai/homework/controllers/PostController.java
+++ server/src/main/java/com/akamai/homework/controllers/PostController.java
@@ -1,11 +1,17 @@
 package com.akamai.homework.controllers;
 
-import com.akamai.homework.dao.entities.User;
+import com.akamai.homework.dao.entities.Post;
 import com.akamai.homework.services.PostService;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -18,35 +24,70 @@ public class PostController {
     public PostController(PostService postService) {
         this.postService = postService;
     }
+
     @PostMapping("/addPost")
-    public String addPost(@RequestParam(name = "userId") String userId, @RequestParam(name = "postTitle") String postTitle,
+    public ResponseEntity<String> addPost(@RequestParam(name = "userId") String userId, @RequestParam(name = "postTitle") String postTitle,
                           @RequestParam(name = "postText") String postText) {
-        return postService.addPost(userId, postTitle, postText);
+        try {
+            return ResponseEntity.ok(postService.addPost(userId, postTitle, postText));
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @PostMapping("/editPost")
-    public String editPostText(@RequestParam(name = "id") String id,
+    public ResponseEntity<String> editPostText(@RequestParam(name = "id") String id,
                                @RequestParam(name = "newText") String newText) {
-        return postService.editPost(id, newText);
+        try {
+            return  ResponseEntity.ok(postService.editPostText(id, newText));
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
+    @Transactional
     @PostMapping("/upvote")
-    public String upvotingPost(@RequestParam(name = "id") String id, @RequestParam(name = "upvoted_by") String userId) {
-        return postService.upvotingPost(id, userId);
+    public ResponseEntity<String> upvotingPost(@RequestParam(name = "id") String id, @RequestParam(name = "upvoted_by") String userId) {
+        try {
+            return ResponseEntity.ok(postService.upvotingPost(id, userId));
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
+    @Transactional
     @PostMapping("/downvote")
-    public String downvotingPost(@RequestParam(name = "id") String id, @RequestParam(name = "downvote_by") String userId) {
-        return postService.downvotingPost(id, userId);
+    public ResponseEntity<String> downvotingPost(@RequestParam(name = "id") String id, @RequestParam(name = "downvote_by") String userId) {
+        try {
+            return ResponseEntity.ok(postService.downvotingPost(id, userId));
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
+//TODO: remove if not needed
 
-    @GetMapping("/getAllTopics")
-    public List<String> getAllTopics() {
-        return postService.getAllTopics();
-    }
+//    @GetMapping("/getAllTopics")
+//    public List<String> getAllTopics() {
+//        return postService.getAllTopics();
+//    }
+//
+//    @GetMapping("/getText")
+//    public String getTextById(@RequestParam(name = "id") String id) {
+//        return postService.getTextById(id);
+//    }
 
-    @GetMapping("/getText")
-    public String getTextById(@RequestParam(name = "id") String id) {
-        return postService.getTextById(id);
+    @GetMapping("/getPostsSortedAndByPaging")
+    public ResponseEntity<Page<Post>> getPostsSortedAndByPaging(@RequestParam(name = "page", defaultValue = "0") int page,
+                                                                    @RequestParam(name = "size", defaultValue = "10") int size,
+                                                                    @RequestParam(defaultValue = "asc") String sortDirection) {
+        try {
+            Sort sort = Sort.by(sortDirection.equals("asc") ? Sort.Direction.ASC : Sort.Direction.DESC, "createdOn")
+                    .and(Sort.by(sortDirection.equals("asc") ? Sort.Direction.ASC : Sort.Direction.DESC, "points"));
+
+            Pageable pageable = PageRequest.of(page, size, sort);
+            return ResponseEntity.ok(postService.getPostsSortedAndByPaging(pageable));
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
--- server/src/main/java/com/akamai/homework/services/PostService.java
+++ server/src/main/java/com/akamai/homework/services/PostService.java
@@ -6,8 +6,9 @@ import com.akamai.homework.dao.repositories.PostRepository;
 import com.akamai.homework.dao.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,13 +17,16 @@ import java.util.stream.Collectors;
 public class PostService {
     PostRepository postRepository;
     UserRepository userRepository;
+
+    final String UPDATED_POINTS_SUCCESSFULLY_MESSAGE = "updated points successfully";
+    final String COULD_NOT_UPDATE_POINTS_SUCCESSFULLY_MESSAGE = "could not update points: ";
     @Autowired
     public PostService(PostRepository postRepository, UserRepository userRepository) {
         this.postRepository = postRepository;
         this.userRepository = userRepository;
     }
 
-    public String addPost(String userId, String postTitle, String postText) {
+    public String addPost(String userId, String postTitle, String postText) throws Exception {
         log.info("adding new post to the system: {} for user id: {}", postTitle, userId);
         try {
             Post newPost = new Post(userId, postTitle, postText);
@@ -31,71 +35,85 @@ public class PostService {
             newPost.setUser(user);
             postRepository.saveAndFlush(newPost);
         } catch (Exception e) {
-            log.error("could not add new post: {}", e.toString());
+            throw new Exception("could not add new post: " + e);
         }
-        return "Saved";
+        return "new post added successfully";
     }
 
-    public String editPost(String id, String newText) {
-        log.info("edit post's text: {}", newText);
+    public String editPostText(String id, String newText) throws Exception {
+        log.info("edit post's: {} text: {}", id, newText);
         try {
             postRepository.updateTextById(Long.parseLong(id), newText);
             postRepository.flush();
         } catch (Exception e) {
-            log.error("could not update post: {}", e.toString());
+            throw new Exception("could not update post: " + e);
         }
 
-        return "Saved";
+        return "post's text edit successfully";
     }
 
-    public String upvotingPost (String id, String userId) {
+    //TODO: update user did that action in the updated by field
+    public String upvotingPost (String id, String userId) throws Exception {
         log.info("upvoting a post: {} by user: {}", id, userId); //TODO: change to userName from Request
 
         try {
+            // TODO: add new field in entity: upvotes
             int currentPoints = postRepository.selectPointsById(Long.parseLong(id));
             log.info("current points: {}", currentPoints);
             currentPoints++;
             postRepository.updatePointsById(Long.parseLong(id), currentPoints);
             postRepository.flush();
         } catch (Exception e) {
-            log.error("could not update points: {}", e.toString());
+            throw new Exception(COULD_NOT_UPDATE_POINTS_SUCCESSFULLY_MESSAGE + e);
         }
 
-        return "Saved";
+        return UPDATED_POINTS_SUCCESSFULLY_MESSAGE;
     }
 
-    public String downvotingPost(String id, String userId) {
+    //TODO: update user did that action in the updated by field
+    public String downvotingPost(String id, String userId) throws Exception {
         log.info("downvotingPost a post: {} by user: {}", id, userId);
 
         try {
             int currentPoints = postRepository.selectPointsById(Long.parseLong(id));
             log.info("current points: {}", currentPoints);
+            // TODO: add new field in entity: downvotes
             if (currentPoints > 0)  {
                 currentPoints--;
                 postRepository.updatePointsById(Long.parseLong(id), currentPoints);
                 postRepository.flush();
             }
         } catch (Exception e) {
-            log.error("could not update points: {}", e.toString());
+            throw new Exception(COULD_NOT_UPDATE_POINTS_SUCCESSFULLY_MESSAGE + e);
         }
 
-        return "Saved";
+        return UPDATED_POINTS_SUCCESSFULLY_MESSAGE;
     }
+//TODO: remove if not needed!!!
 
-    public List<String> getAllTopics() {
-        return postRepository.findAll().stream()
-        .map(Post::getTitle)
-        .distinct()
-        .collect(Collectors.toList());
-    }
+//    public List<String> getAllTopics() {
+//        return postRepository.findAll().stream()
+//        .map(Post::getTitle)
+//        .distinct()
+//        .collect(Collectors.toList());
+//    }
+//
+//    public String getTextById(String id) {
+//        try {
+//            return postRepository.selectTextById(Long.parseLong(id));
+//        } catch (Exception e) {
+//            log.error("could not get text for post: {}, {}", id, e.toString());
+//        }
+//
+//        return null;
+//    }
 
-    public String getTextById(String id) {
+    public Page<Post> getPostsSortedAndByPaging(Pageable pageable) throws Exception {
         try {
-            return postRepository.selectTextById(Long.parseLong(id));
+            log.info("trying to receive posts by paging");
+            return postRepository.findAll(pageable);
         } catch (Exception e) {
-            log.error("could not get text for post: {}, {}", id, e.toString());
+            throw new Exception("could not get posts: " + e);
         }
-
-        return null;
     }
 }
